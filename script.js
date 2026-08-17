2e81c5e6497d222d3309977345c1056983eadc4c
Gabriel <gabrielmoraisreis2@gmail.com>
2026-08-17 18:28:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function compra(linkDestino) {
+
+    alert('Assinatura feita com sucesso!');
+
+    window.open(linkDestino, '_blank');
+
+}
+
+function darkview(linkDestino) {
+
+    window.open(linkDestino, '_blank');
+
+}
+
+function questi() {
+
+    alert('Questionário enviado!');
+
+}
